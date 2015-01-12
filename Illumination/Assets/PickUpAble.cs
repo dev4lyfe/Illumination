5b9f3567cfc7840e960acd49ef056f0b085ac9ba
Pablo Leon-Luna <paleonluna@gmail.com>
2015-01-11 20:34:44 -0800

    someshit

--- Illumination/Assets/PickUpAble.cs
+++ Illumination/Assets/PickUpAble.cs
@@ -7,13 +7,20 @@ public class PickUpAble : MonoBehaviour {
 	public bool isPickedUp = false;
 
 	void OnTriggerEnter (Collider other) {
-		if (other.tag == "Camera")
+		if (other.tag == "MainCamera")
 			canBePickedUp = true;
 	}
 
 	void OnTriggerExit (Collider other) {
-		if (other.tag == "Camera")
+		if (other.tag == "MainCamera")
 			canBePickedUp = false;
+	}
+
+	void OnMouseDown () {
+		if (canBePickedUp) {
+			gameObject.transform.SetParent(GameObject.FindGameObjectWithTag("MainCamera").transform);
+			
+		}
 	}
 
 }
