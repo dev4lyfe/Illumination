d9bb7cae0f20a1c5ea03e5f106050a7e7d93acc2
Pablo Leon-Luna <paleonluna@gmail.com>
2015-01-11 20:11:17 -0800

    micro push

---
+++ Illumination/Assets/PickUpAble.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickUpAble : MonoBehaviour {
+
+	public bool canBePickedUp = false;
+	public bool isPickedUp = false;
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Camera")
+			canBePickedUp = true;
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.tag == "Camera")
+			canBePickedUp = false;
+	}
+
+}
