6abe6671d3573c4e05499da09a15589d859b5cf5
Pablo Leon-Luna <paleonluna@gmail.com>
2015-01-19 17:27:38 -0800

    cubes

---
+++ Illumination/Assets/GridLayout.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GridLayout : MonoBehaviour {
+
+	public int rows, columns;
+	public GameObject cubePrefab;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < rows * columns; i++) {
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+}
