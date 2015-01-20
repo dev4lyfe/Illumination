4883e0d56d259012616999e61c13843d7dd7c3b3
Pablo Leon-Luna <paleonluna@gmail.com>
2015-01-20 14:57:30 -0800

    working on programming

--- Illumination/Assets/GridLayout.cs
+++ Illumination/Assets/GridLayout.cs
@@ -5,17 +5,28 @@ public class GridLayout : MonoBehaviour {
 
 	public int rows, columns;
 	public GameObject cubePrefab;
-
+	public float distanceBetweenCubes;
 	// Use this for initialization
 	void Start () {
-		for (int i = 0; i < rows * columns; i++) {
-
-		}
+		SpawnLayout ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+
+	void SpawnLayout () {
+		for (int i = 0; i < rows * columns; i++) {
+			int yPosition = Mathf.CeilToInt(i / columns); //returns the row into which the cube fits into
+			int xPosition = i % rows; //returns which column the cube fits into
+			Vector3 cubePosition = distanceBetweenCubes * (new Vector3(xPosition, 0, yPosition));
+			GameObject newCube = (GameObject)Instantiate(cubePrefab, cubePosition, Quaternion.identity);
+			newCube.transform.parent = transform;
+		}
+
+	}
+
+	public void SetCamera (Camera orthoCamera){}
 
 }
