41206ae7f52ce3454cf7de8abd2c1f50647c6a43
Mike Dukowski <mike@virsix.com>
2015-01-19 17:29:54 -0800

    CameraScript

---
+++ Illumination/Assets/PlayerControl.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public float speed = 3.0f;
+	public float rotateSpeed = 3.0f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		CharacterController controller = GetComponent<CharacterController> ();
+		transform.Rotate (0, Input.GetAxis ("Horizontal") * rotateSpeed, 0);
+		Vector3 forward = transform.TransformDirection (Vector3.forward);
+		float curSpeed = speed * Input.GetAxis ("Vertical");
+		controller.SimpleMove (forward * curSpeed);
+	
+	}
+}
